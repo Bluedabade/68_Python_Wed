826a77eec40e5a0e3db9b1350f728b796fddebb6
Bluedabade <tijara2525@gmail.com>
2025-07-16 11:49:56 +0700

    EXERCISE-III

--- Lecture-04/EXERCISE-III.py
+++ Lecture-04/EXERCISE-III.py
@@ -1,13 +1,19 @@
 max = 100
 rows = int(input("How many rows do you want to display number:"))
-count = max / rows
+count = max // rows
 num = 0;
 for i in range(int((max / count))):
-    for j in range(0, int(count)+1):
+    for j in range(0, int(count)):
         num+=1
         if num > max: 
             continue
         print(num,",", end="")
-    print("\n",i+1)
+    if max % rows != 0 and i == int((max / count)-1):
+        for k in range(num,(max)):
+            num+=1
+            print(num,",", end="")
+    print("\n")
+        
+
 
 #int((max / count))
